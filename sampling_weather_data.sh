cf33354163b7b654d89fa255cef0a200b080d554
Labu Siam <90373385+labusiam@users.noreply.github.com>
2022-04-26 21:12:28 +0800

    Update sampling_weather_data.sh

--- sampling_weather_data.sh
+++ sampling_weather_data.sh
@@ -11,9 +11,13 @@ else
 fi
 
 # Seleksi Data berdasarkan kolom
-
 csvcut -c 1,2,3,4 ./data/KJAX.csv | csvgrep 2014 > data/KJAX_sliced.csv 
 
 # Sampling Data
 cat data/KJAX_sliced.csv | sample -r 0.2 > data/KJAX_sampled.csv
+
+if [ ! -d log ]; then
+	mkdir log
+	touch log/log
+else
 
