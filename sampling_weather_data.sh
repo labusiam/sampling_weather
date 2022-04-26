e71ae76bad657229f28e3e681832870ed08e3b07
Faizal <Faizal@pacmannai.com>
2022-04-26 20:50:09 +0800

    Menambahkan file data di dalam folder data dan file sampling_weather_data.sh

---
+++ sampling_weather_data.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+# Download Data
+
+# cek folder data
+if [ ! -d data ]; then
+	mkdir data
+	mv KJAX.csv data/
+else
+	mv KJAX.csv data/
+fi
+
+# Seleksi Data berdasarkan kolom
+
+csvcut -c 1,2,3,4 ./data/KJAX.csv > data/KJAX_sliced.csv 
+
+# Sampling Data
+cat data/KJAX_sliced.csv | sample -r 0.2 > data/KJAX_sampled.csv
